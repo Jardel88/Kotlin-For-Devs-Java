40dcd500e22dd9dbd6d424900bbce63dadc61370
jardel88 <jardelsilvagomes@hotmail.com>
2022-07-05 13:19:12 -0300

    Projeto referencia Java

---
+++ Aplicacao Java para Kotlin/Projeto referencia Java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(5, 'Porbandar - India', 'Indira', 'Female', 'Gandhi'),
+	(7, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(8, 'Kentucky - United States', 'Muhammad', 'Male', 'Ali'),
+	(9, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
